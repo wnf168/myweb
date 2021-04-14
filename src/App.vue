252dbc25f0991b20a418cdb5828eab4b1f9127b2
wnf168 <327634465@qq.com>
2021-04-14 14:55:00 +0800

    样式及elementui

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    {{ a }}
-    <button @click="test">click</button>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
     <router-view />
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import Component from 'vue-class-component'
-@Component
-export default class App extends Vue {
-  a = '1'
-  test() {
-    console.log('abc')
-  }
 
-  created() {
-    console.log(9969)
-  }
-}
+export default Vue.extend({
+  name: 'App'
+})
 </script>
 <style lang="scss" scoped></style>
